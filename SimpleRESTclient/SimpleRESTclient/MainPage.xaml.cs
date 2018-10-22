84895b24b7373a70cf60b3bb4d926f049820b42f
krisophertab <krzysiektab@gmail.com>
2018-10-22 15:21:11 -0700

    weather demo: data from openweathermap.org, simple chart with fixed data

--- SimpleRESTclient/SimpleRESTclient/MainPage.xaml.cs
+++ SimpleRESTclient/SimpleRESTclient/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Newtonsoft.Json;
+using Microcharts;
+using SkiaSharp;
 
 namespace SimpleRESTclient
 {
@@ -13,37 +15,100 @@ namespace SimpleRESTclient
         public MainPage()
         {
             InitializeComponent();
+
+            fillDefaultPathEntry();
+            showMicrochartsExample();
+            
         }
 
-        public void mainPageSetOutputText(String text)
+        public void fillDefaultPathEntry()
         {
-            outputLabel.Text = text;
+            //Sign up for a free API key at http://openweathermap.org/appid  
+            string key = "8812761dc6a41a9f49d8e0290394c618";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?id=3094802&appid=" + key + "&units=imperial";
+
+            this.pathEntry.Text = queryString;
         }
 
-        private void triggerButton_Clicked(object sender, EventArgs e)
+        public void showMicrochartsExample()
         {
-            String something = App.service.getResoultOfSomething();
-            mainPageSetOutputText(something);
+            var entries = new[]
+             {
+                 new Microcharts.Entry(-4)
+                 {
+                     Label = "1"
+                 },
+                 new Microcharts.Entry(2)
+                 {
+                     Label = "2",
+                 },
+                 new Microcharts.Entry(5)
+                 {
+                     Label = "3"
+                 },
+                 new Microcharts.Entry(10)
+                 {
+                     Label = "4",
+                 },
+                 new Microcharts.Entry(15)
+                 {
+                     Label = "5"
+                 },
+                 new Microcharts.Entry(22)
+                 {
+                     Label = "6",
+                 },
+                 new Microcharts.Entry(25)
+                 {
+                     Label = "7"
+                 },
+                 new Microcharts.Entry(22)
+                 {
+                     Label = "8",
+                 },
+                 new Microcharts.Entry(15)
+                 {
+                     Label = "9"
+                 },
+                 new Microcharts.Entry(6)
+                 {
+                     Label = "10",
+                 },
+                 new Microcharts.Entry(0)
+                 {
+                     Label = "11"
+                 },
+                 new Microcharts.Entry(-3)
+                 {
+                     Label = "12",
+                 },
+
+            };
+
+            var chart = new LineChart()
+            {
+                Entries = entries,
+                LineMode = LineMode.Straight,
+                LineSize = 8,
+            };
+
+            this.chartView.Chart = chart;
         }
 
-        private void getBtn_Clicked(object sender, EventArgs e)
+        private async Task nextBtn_ClickedAsync(object sender, EventArgs e)
         {
-            App.service.GetRest(urlEntry.Text);
-            String response = App.service.GetRestResoult();
-            mainPageSetOutputText(response);
+            if (!String.IsNullOrEmpty(pathEntry.Text))
+            {
+                Weather weather = await WetherService.GetWeather(pathEntry.Text);
+                outputLabel.Text = weather.getWetherString();
+            }
+            else
+            {
+                outputLabel.Text = "unexpected error, sorry";
+            }
         }
 
-        private void postBtn_Clicked(object sender, EventArgs e)
-        {
-            App.service.PostRest();
-        }
-
-        private void putBtn_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
-        private void deleteBtn_Clicked(object sender, EventArgs e)
+        private void nextBtn_Clicked(object sender, EventArgs e)
         {
 
         }
---
+++ SimpleRESTclient/SimpleRESTclient/WetherService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleRESTclient
+{
+    class WetherService
+    {
+
+        public static async Task<Weather> GetWeather(string path)
+        {
+            
+            dynamic results = await DataService.GetDataFromService(path).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Id = (string)results["id"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                weather.Date = sunrise.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+    }
+
+    public class Weather
+    {
+        // Because labels bind to these values, set them to an empty string to
+        // ensure that the label appears on all platforms by default.
+        public string Id { get; set; } = " ";
+        public string Temperature { get; set; } = " ";
+        public string Date{ get; set; } = " ";
+
+        public string getWetherString()
+        {
+            string retStr = " ";
+            retStr += " Id :" + this.Id;
+            retStr += " Temp :" + this.Temperature;
+            retStr += " Date :" + this.Date;
+
+            return retStr;
+        }
+    }
+
+
+
+
+}
